faf3162d5e79a6214a90da232b172076f020a86d
Bobby Naluz <bnaluz9@gmail.com>
2024-01-24 20:58:05 -0500

    phases done - Bonus

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,20 @@
+import { changeTitle, changeHeader } from './manipulate.js';
+import changeAboutMe from './manipulate.js';
+
+import {
+  findElementById,
+  findFirstElementOfTag,
+  findFirstElementOfClass,
+  findElementsOfTag,
+} from './search.js';
+
+import findElementsOfClass from './search.js';
+
 changeTitle();
 changeHeader();
 changeAboutMe();
-console.log(findElementById("header")); // <div id="header">
-console.log(findFirstElementOfTag("LI")); // <li><h3>Computer Science</h3>...</li>
-console.log(findFirstElementOfClass("section")); // <div class="section"><h2>About Me</h2>...</div>
-console.log(findElementsOfTag("H3")); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
-console.log(findElementsOfClass("section")); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
+console.log(findElementById('header')); // <div id="header">
+console.log(findFirstElementOfTag('LI')); // <li><h3>Computer Science</h3>...</li>
+console.log(findFirstElementOfClass('section')); // <div class="section"><h2>About Me</h2>...</div>
+console.log(findElementsOfTag('H3')); // [h3, h3, h3, h3, h3, h3, h3, h3, h3, h3, h3] (length: 11)
+console.log(findElementsOfClass('section')); // [div.section, div.section, div.section, div.section, div.section, div.section] (length: 6)
--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,27 @@
-function changeTitle() {
-    // Change the title of the page to "(Your name)'s Portfolio"
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
+  // Your code here
 
-    // Your code here 
+  document.title = 'our portfolio';
+  return;
 }
 
-function changeHeader() {
-    // Change the name in the h1 of the page to your name
-
-    // Your code here 
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
+  // Your code here
+  document.querySelector('h1').innerText = 'Hi we are namestest';
+  return;
 }
 
 function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
+  /* Update the first paragraph in the About Me section with a small
      passage about yourself */
+  // Your code here
 
-    // Your code here 
+  let aboutMeP = document.querySelectorAll('.section > p');
+  console.log(aboutMeP);
+  aboutMeP[0].innerText = 'my first paragraph';
+  aboutMeP[1].innerText = 'my second paragraph';
 }
+
+export default changeAboutMe;
